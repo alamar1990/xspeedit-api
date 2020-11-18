eb9330c616ff47ba2b9703dfad93013acf88b041
Alain Marquez <alamar1990@gmail.com>
2020-11-17 21:09:53 -0500

    Fix and validate endpoints

--- index.js
+++ index.js
@@ -10,10 +10,22 @@ app.listen(3000, () => {
     console.log("Server init on port 3000");
 });
 
-app.post('/processItems', function (req, res) {
+app.get('/', (req, res) => {
+    res.send('Please POST to /processItems to process Items \n' +
+        'using this example parameters \n' +
+        '{\n' +
+        '    "items_string" : "94861616"\n' +
+        '}');
+});
+
+app.post('/processItems', (req, res) => {
     const itemsString = req.body.items_string
     if (!itemsString){
         res.send('No items sent');
+        return
+    }
+    if (typeof itemsString === 'number'){
+        res.send('Numbers are not accepted');
         return
     }
 
