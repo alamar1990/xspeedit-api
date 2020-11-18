929071da9b440890be570586a5a816e01b5f6eaa
Alain Marquez <alamar1990@gmail.com>
2020-11-17 20:19:46 -0500

    Add Post funcionality to express server for simple API

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const Robot = require('./src/xspeedit/Xspeedit')
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
     console.log("Server init on port 3000");
 });
 
-app.get('/', function (req, res) {
-    res.send('Greetings from express');
+app.post('/processItems', function (req, res) {
+    const itemsString = req.body.items_string
+    if (!itemsString){
+        res.send('No items sent');
+        return
+    }
+
+    const robot = new Robot(itemsString)
+    const boxString = robot.getBoxString()
+    const boxCount = robot.getBoxCount()
+
+    res.send({
+        boxedItems: boxString,
+        boxCount: boxCount    });
 });
